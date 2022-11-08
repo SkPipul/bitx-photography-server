c1560dd96a79bdb9902d0ab1f571ae15083f07fd
SkPipul <skjabed252@gmail.com>
2022-11-08 22:23:51 +0600

    server is running

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors ())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zgj4c3m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Bitx photography server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Bitx server is running on ${port}`);
+})
